444960d200387ac652f987065efad3295504a132
John Luo <johluo@microsoft.com>
2021-04-22 13:06:23 -0700

    Fix some tests

--- src/Servers/Kestrel/shared/test/EventRaisingResourceCounter.cs
+++ src/Servers/Kestrel/shared/test/EventRaisingResourceCounter.cs
@@ -2,33 +2,62 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Internal;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
 
 namespace Microsoft.AspNetCore.Server.Kestrel.Tests
 {
-    internal class EventRaisingResourceCounter : ResourceCounter
+    internal class EventRaisingResourceLimiter : IResourceLimiter
     {
-        private readonly ResourceCounter _wrapped;
+        private readonly IResourceLimiter _wrapped;
 
-        public EventRaisingResourceCounter(ResourceCounter wrapped)
+        public EventRaisingResourceLimiter(IResourceLimiter wrapped)
         {
             _wrapped = wrapped;
         }
 
+        public long EstimatedCount => _wrapped.EstimatedCount;
+
         public event EventHandler OnRelease;
         public event EventHandler<bool> OnLock;
 
-        public override void ReleaseOne()
+        public ValueTask<IResource> AcquireAsync(long requestedCount, CancellationToken cancellationToken = default)
         {
-            _wrapped.ReleaseOne();
-            OnRelease?.Invoke(this, EventArgs.Empty);
+            throw new NotImplementedException();
         }
 
-        public override bool TryLockOne()
+        public bool TryAcquire(long requestedCount, out IResource resource)
         {
-            var retVal = _wrapped.TryLockOne();
+            var retVal = _wrapped.TryAcquire(out var innerResource);
+            resource = new WrappedResource(innerResource, () => OnRelease.Invoke(this, null));
             OnLock?.Invoke(this, retVal);
             return retVal;
+        }
+
+        private class WrappedResource : IResource
+        {
+            private readonly IResource _resource;
+            private readonly Action _releaseAction;
+
+            public WrappedResource(IResource resource, Action releaseAction)
+            {
+                _resource = resource;
+                _releaseAction = releaseAction;
+            }
+
+            public override void Release(long requestedCount)
+            {
+                _resource.Release(requestedCount);
+                _releaseAction();
+            }
+
+            // Careful with locking in dispose
+            public override void Dispose()
+            {
+                _resource.Release(_resource.Count);
+            }
         }
     }
 }
