b7cb41ae231bd1077c9d626f3df498f8589d0801
Tanay Parikh <taparik@microsoft.com>
2021-12-01 06:55:46 -0800

    Update TestRunner.cs

--- eng/helix/content/RunTests/TestRunner.cs
+++ eng/helix/content/RunTests/TestRunner.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 #if INSTALLPLAYWRIGHT
