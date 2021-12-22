8784b31f15c24d989e5050dc8ec8dd7dab3cc78d
Tanay Parikh <TanayParikh@users.noreply.github.com>
2021-12-22 17:02:25 +0000

    License and Fix tsdoc-param-tag-missing-hyphen

--- src/JSInterop/Microsoft.JSInterop.JS/src/src/Microsoft.JSInterop.ts
+++ src/JSInterop/Microsoft.JSInterop.JS/src/src/Microsoft.JSInterop.ts
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 // This is a single-file self-contained module to avoid the need for a Webpack build
 
 export module DotNet {
@@ -81,7 +84,7 @@ export module DotNet {
    * Sets the specified .NET call dispatcher as the current instance so that it will be used
    * for future invocations.
    *
-   * @param dispatcher An object that can dispatch calls from JavaScript to a .NET runtime.
+   * @param dispatcher - An object that can dispatch calls from JavaScript to a .NET runtime.
    */
   export function attachDispatcher(dispatcher: DotNetCallDispatcher) {
     dotNetDispatcher = dispatcher;
@@ -89,7 +92,7 @@ export module DotNet {
 
   /**
    * Adds a JSON reviver callback that will be used when parsing arguments received from .NET.
-   * @param reviver The reviver to add.
+   * @param reviver - The reviver to add.
    */
   export function attachReviver(reviver: JsonReviver) {
     jsonRevivers.push(reviver);
@@ -99,9 +102,9 @@ export module DotNet {
    * Invokes the specified .NET public method synchronously. Not all hosting scenarios support
    * synchronous invocation, so if possible use invokeMethodAsync instead.
    *
-   * @param assemblyName The short name (without key/version or .dll extension) of the .NET assembly containing the method.
-   * @param methodIdentifier The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
-   * @param args Arguments to pass to the method, each of which must be JSON-serializable.
+   * @param assemblyName - The short name (without key/version or .dll extension) of the .NET assembly containing the method.
+   * @param methodIdentifier - The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
+   * @param args - Arguments to pass to the method, each of which must be JSON-serializable.
    * @returns The result of the operation.
    */
   export function invokeMethod<T>(assemblyName: string, methodIdentifier: string, ...args: any[]): T {
@@ -111,9 +114,9 @@ export module DotNet {
   /**
    * Invokes the specified .NET public method asynchronously.
    *
-   * @param assemblyName The short name (without key/version or .dll extension) of the .NET assembly containing the method.
-   * @param methodIdentifier The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
-   * @param args Arguments to pass to the method, each of which must be JSON-serializable.
+   * @param assemblyName - The short name (without key/version or .dll extension) of the .NET assembly containing the method.
+   * @param methodIdentifier - The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
+   * @param args - Arguments to pass to the method, each of which must be JSON-serializable.
    * @returns A promise representing the result of the operation.
    */
   export function invokeMethodAsync<T>(assemblyName: string, methodIdentifier: string, ...args: any[]): Promise<T> {
@@ -123,7 +126,7 @@ export module DotNet {
   /**
    * Creates a JavaScript object reference that can be passed to .NET via interop calls.
    *
-   * @param jsObject The JavaScript Object used to create the JavaScript object reference.
+   * @param jsObject - The JavaScript Object used to create the JavaScript object reference.
    * @returns The JavaScript object reference (this will be the same instance as the given object).
    * @throws Error if the given value is not an Object.
    */
@@ -146,7 +149,7 @@ export module DotNet {
   /**
    * Creates a JavaScript data reference that can be passed to .NET via interop calls.
    *
-   * @param streamReference The ArrayBufferView or Blob used to create the JavaScript stream reference.
+   * @param streamReference - The ArrayBufferView or Blob used to create the JavaScript stream reference.
    * @returns The JavaScript data reference (this will be the same instance as the given object).
    * @throws Error if the given value is not an Object or doesn't have a valid byteLength.
    */
@@ -188,7 +191,7 @@ export module DotNet {
   /**
    * Disposes the given JavaScript object reference.
    *
-   * @param jsObjectReference The JavaScript Object reference.
+   * @param jsObjectReference - The JavaScript Object reference.
    */
   export function disposeJSObjectReference(jsObjectReference: any): void {
     const id = jsObjectReference && jsObjectReference[jsObjectIdKey];
@@ -201,7 +204,7 @@ export module DotNet {
   /**
    * Parses the given JSON string using revivers to restore args passed from .NET to JS.
    *
-   * @param json The JSON stirng to parse.
+   * @param json - The JSON stirng to parse.
    */
   function parseJsonWithRevivers(json: string): any {
     return json ? JSON.parse(json, (key, initialValue) => {
@@ -290,10 +293,10 @@ export module DotNet {
     /**
      * Optional. If implemented, invoked by the runtime to perform a synchronous call to a .NET method.
      *
-     * @param assemblyName The short name (without key/version or .dll extension) of the .NET assembly holding the method to invoke. The value may be null when invoking instance methods.
-     * @param methodIdentifier The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
-     * @param dotNetObjectId If given, the call will be to an instance method on the specified DotNetObject. Pass null or undefined to call static methods.
-     * @param argsJson JSON representation of arguments to pass to the method.
+     * @param assemblyName - The short name (without key/version or .dll extension) of the .NET assembly holding the method to invoke. The value may be null when invoking instance methods.
+     * @param methodIdentifier - The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
+     * @param dotNetObjectId - If given, the call will be to an instance method on the specified DotNetObject. Pass null or undefined to call static methods.
+     * @param argsJson - JSON representation of arguments to pass to the method.
      * @returns JSON representation of the result of the invocation.
      */
     invokeDotNetFromJS?(assemblyName: string | null, methodIdentifier: string, dotNetObjectId: number | null, argsJson: string): string | null;
@@ -301,27 +304,27 @@ export module DotNet {
     /**
      * Invoked by the runtime to begin an asynchronous call to a .NET method.
      *
-     * @param callId A value identifying the asynchronous operation. This value should be passed back in a later call from .NET to JS.
-     * @param assemblyName The short name (without key/version or .dll extension) of the .NET assembly holding the method to invoke. The value may be null when invoking instance methods.
-     * @param methodIdentifier The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
-     * @param dotNetObjectId If given, the call will be to an instance method on the specified DotNetObject. Pass null to call static methods.
-     * @param argsJson JSON representation of arguments to pass to the method.
+     * @param callId - A value identifying the asynchronous operation. This value should be passed back in a later call from .NET to JS.
+     * @param assemblyName - The short name (without key/version or .dll extension) of the .NET assembly holding the method to invoke. The value may be null when invoking instance methods.
+     * @param methodIdentifier - The identifier of the method to invoke. The method must have a [JSInvokable] attribute specifying this identifier.
+     * @param dotNetObjectId - If given, the call will be to an instance method on the specified DotNetObject. Pass null to call static methods.
+     * @param argsJson - JSON representation of arguments to pass to the method.
      */
     beginInvokeDotNetFromJS(callId: number, assemblyName: string | null, methodIdentifier: string, dotNetObjectId: number | null, argsJson: string): void;
 
     /**
      * Invoked by the runtime to complete an asynchronous JavaScript function call started from .NET
      *
-     * @param callId A value identifying the asynchronous operation.
-     * @param succeded Whether the operation succeeded or not.
-     * @param resultOrError The serialized result or the serialized error from the async operation.
+     * @param callId - A value identifying the asynchronous operation.
+     * @param succeded - Whether the operation succeeded or not.
+     * @param resultOrError - The serialized result or the serialized error from the async operation.
      */
     endInvokeJSFromDotNet(callId: number, succeeded: boolean, resultOrError: any): void;
 
     /**
      * Invoked by the runtime to transfer a byte array from JS to .NET.
-     * @param id The identifier for the byte array used during revival.
-     * @param data The byte array being transferred for eventual revival.
+     * @param id - The identifier for the byte array used during revival.
+     * @param data - The byte array being transferred for eventual revival.
      */
      sendByteArray(id: number, data: Uint8Array): void;
   }
@@ -333,8 +336,8 @@ export module DotNet {
     /**
      * Finds the JavaScript function matching the specified identifier.
      *
-     * @param identifier Identifies the globally-reachable function to be returned.
-     * @param targetInstanceId The instance ID of the target JS object.
+     * @param identifier - Identifies the globally-reachable function to be returned.
+     * @param targetInstanceId - The instance ID of the target JS object.
      * @returns A Function instance.
      */
     findJSFunction, // Note that this is used by the JS interop code inside Mono WebAssembly itself
@@ -342,18 +345,18 @@ export module DotNet {
     /**
      * Disposes the JavaScript object reference with the specified object ID.
      *
-     * @param id The ID of the JavaScript object reference.
+     * @param id - The ID of the JavaScript object reference.
      */
     disposeJSObjectReferenceById,
 
     /**
      * Invokes the specified synchronous JavaScript function.
      *
-     * @param identifier Identifies the globally-reachable function to invoke.
-     * @param argsJson JSON representation of arguments to be passed to the function.
-     * @param resultType The type of result expected from the JS interop call.
-     * @param targetInstanceId The instance ID of the target JS object.
-     * @returns JSON representation of the invocation result.
+     * @param identifier - Identifies the globally-reachable function to invoke.
+     * @param argsJson - JSON representation of arguments to be passed to the function.
+     * @param resultType - The type of result expected from the JS interop call.
+     * @param targetInstanceId - The instance ID of the target JS object.
+     * @returns - JSON representation of the invocation result.
      */
     invokeJSFromDotNet: (identifier: string, argsJson: string, resultType: JSCallResultType, targetInstanceId: number) => {
       const returnValue = findJSFunction(identifier, targetInstanceId).apply(null, parseJsonWithRevivers(argsJson));
@@ -367,11 +370,11 @@ export module DotNet {
     /**
      * Invokes the specified synchronous or asynchronous JavaScript function.
      *
-     * @param asyncHandle A value identifying the asynchronous operation. This value will be passed back in a later call to endInvokeJSFromDotNet.
-     * @param identifier Identifies the globally-reachable function to invoke.
-     * @param argsJson JSON representation of arguments to be passed to the function.
-     * @param resultType The type of result expected from the JS interop call.
-     * @param targetInstanceId The ID of the target JS object instance.
+     * @param asyncHandle - A value identifying the asynchronous operation. This value will be passed back in a later call to endInvokeJSFromDotNet.
+     * @param identifier - Identifies the globally-reachable function to invoke.
+     * @param argsJson - JSON representation of arguments to be passed to the function.
+     * @param resultType - The type of result expected from the JS interop call.
+     * @param targetInstanceId - The ID of the target JS object instance.
      */
     beginInvokeJSFromDotNet: (asyncHandle: number, identifier: string, argsJson: string, resultType: JSCallResultType, targetInstanceId: number): void => {
       // Coerce synchronous functions into async ones, plus treat
@@ -394,9 +397,9 @@ export module DotNet {
 
     /**
      * Receives notification that an async call from JS to .NET has completed.
-     * @param asyncCallId The identifier supplied in an earlier call to beginInvokeDotNetFromJS.
-     * @param success A flag to indicate whether the operation completed successfully.
-     * @param resultJsonOrExceptionMessage Either the operation result as JSON, or an error message.
+     * @param asyncCallId - The identifier supplied in an earlier call to beginInvokeDotNetFromJS.
+     * @param success - A flag to indicate whether the operation completed successfully.
+     * @param resultJsonOrExceptionMessage - Either the operation result as JSON, or an error message.
      */
     endInvokeDotNetFromJS: (asyncCallId: string, success: boolean, resultJsonOrExceptionMessage: string): void => {
       const resultOrError = success
@@ -407,8 +410,8 @@ export module DotNet {
 
     /**
      * Receives notification that a byte array is being transferred from .NET to JS.
-     * @param id The identifier for the byte array used during revival.
-     * @param data The byte array being transferred for eventual revival.
+     * @param id - The identifier for the byte array used during revival.
+     * @param data - The byte array being transferred for eventual revival.
      */
     receiveByteArray: (id: number, data: Uint8Array): void => {
       byteArraysToBeRevived.set(id, data);
@@ -417,8 +420,8 @@ export module DotNet {
     /**
     * Supplies a stream of data being sent from .NET.
     *
-    * @param streamId The identifier previously passed to JSRuntime's BeginTransmittingStream in .NET code
-    * @param stream The stream data.
+    * @param streamId - The identifier previously passed to JSRuntime's BeginTransmittingStream in .NET code
+    * @param stream - The stream data.
     */
     supplyDotNetStream: (streamId: number, stream: ReadableStream) => {
       if (pendingDotNetToJSStreams.has(streamId)) {
