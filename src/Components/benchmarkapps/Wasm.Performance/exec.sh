54568c59ae2b1216734c7a1c6d25c4782080be09
Pranav K <prkrishn@hotmail.com>
2021-12-22 11:28:40 -0800

    WIP

--- src/Components/benchmarkapps/Wasm.Performance/exec.sh
+++ src/Components/benchmarkapps/Wasm.Performance/exec.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 /opt/bin/start-selenium-standalone.sh&
-./Wasm.Performance.Driver $StressRunDuration
-
+./app/Wasm.Performance.Driver $StressRunDuration&
+./aot-app/Wasm.Performance.Driver $StressRunDuration
